c469f962e92fc7542d51c0fb3659e3e924aab1eb
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-10-31 23:04:03 -0300

    #3 - create initial user

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const port = process.env.PORT || 3000
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-
+const User = require('./models/user')
 const mongo = process.env.MONGODB || 'mongodb://localhost/noticias'
 
 app.set('views', path.join(__dirname, 'views'))
@@ -12,18 +12,26 @@ app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
+const createInitialUser = async () => {
+  const total = await User.countDocuments({ username: 'system_admin' })
+  if (total === 0) {
+    const user = new User({
+      username: 'system_admin',
+      password: 'byte86'
+    })
+    await user.save()
+    console.log('user CREATED')
+  } else {
+    console.log('create User SKIPPED')
+  }
+}
+
 app.get('/', (req, res) => res.render('index'))
 
 mongoose
   .connect(mongo, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
+    createInitialUser()
     app.listen(port, () => console.log('Listening...'))
   })
   .catch(e => console.log(e))
-
-const User = require('./models/user')
-const user = new User({
-  username: 'john-michael',
-  password: 'abc123'
-})
-user.save(() => console.log('ok'))
